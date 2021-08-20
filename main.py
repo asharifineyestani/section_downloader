ace589c549ea32960e0cf074368ee91fabbc4454
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2021-08-20 21:52:12 +0430

    Added download_images

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from src.SectionDownloader import SectionDownloader
 
 sd = SectionDownloader()
-sd.write_log('Hello World!')
+sd.set_file('test.html')
+sd.set_url('https://google.com/')
+sd.download_images()
--- src/SectionDownloader.py
+++ src/SectionDownloader.py
@@ -1,5 +1,8 @@
-import datetime
 import logging
+import datetime
+from bs4 import BeautifulSoup
+from urllib.request import (urlretrieve)
+import os
 
 
 class SectionDownloader:
@@ -10,6 +13,16 @@ class SectionDownloader:
         'handler': '',
         'logger': ''
     }
+
+    config = {
+        'result_path': './result/'
+    }
+
+    req = {
+        'url': ''
+    }
+
+    file = ''
 
     def write_log(self, log_text):
         """ Write log by print() or logger """
@@ -39,3 +52,23 @@ class SectionDownloader:
                 self.log['logger'].info(log_text)
             except UnicodeEncodeError:
                 print("Your text has unicode problem!")
+
+    def set_url(self, url):
+        self.req['url'] = url
+
+    def set_file(self, file_path):
+        f = open(file_path, 'r')
+        self.file = f.read()
+
+    def download_images(self):
+        soup = BeautifulSoup(self.file, 'html.parser')
+        for image in soup.findAll("img"):
+            image["name"] = image["src"].split("/")[-1]
+            image['path'] = image["src"].replace(image["name"], '')
+            os.makedirs(self.config['result_path'] + image['path'], exist_ok=True)
+
+            self.write_log('Downloading image: ' + image["src"][1:])
+            if image["src"].lower().startswith("http"):
+                urlretrieve(image["src"], self.config['result_path'] + image["src"][1:])
+            else:
+                urlretrieve(self.req['url'] + image["src"], self.config['result_path'] + image["src"][1:])
